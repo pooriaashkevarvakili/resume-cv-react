cfa4e6306af2fb45a29b9b7078eba8582a9d1882
pooriavakili09 <pooriavakili09@gmail.com>
2023-06-16 13:08:25 +0330

    updatecode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { I18nextProvider } from 'react-i18next';
+import ReactPDF from '@react-pdf/renderer';
 import "./assets/tailwind.css"
+import i18n from "./i18n.js"
 import "./assets/iransans.css"
+import PrintableComponent from './PrintableComponent.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+
   </React.StrictMode>,
 )
+ReactPDF.render(<PrintableComponent />);
